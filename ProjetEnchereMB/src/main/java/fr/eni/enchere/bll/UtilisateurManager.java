54891507303a770d9277ea3d8600a11238f883a4
MarlonBas <drefir42@gmail.com>
2023-03-21 15:31:11 +0100

    DAOFactory et Utilisateur Manager v1

---
+++ ProjetEnchereMB/src/main/java/fr/eni/enchere/bll/UtilisateurManager.java
@@ -0,0 +1,51 @@
+package fr.eni.enchere.bll;
+
+import fr.eni.enchere.bo.Utilisateur;
+import fr.eni.enchere.dal.DAOFactory;
+import fr.eni.enchere.dal.UtilisateurDAO;
+
+public class UtilisateurManager {
+	
+	// LE SINGLETON
+	// permet de n'avoir qu'un objet UtilisateurManager
+	private static UtilisateurManager instance = null;
+	private UtilisateurDAO utilisateurDAO;
+	
+	private UtilisateurManager(UtilisateurDAO utilisateurDAO) {
+		this.utilisateurDAO = utilisateurDAO;
+	}
+	
+	public static UtilisateurManager getInstance() {
+		if (instance == null) {
+			instance = new UtilisateurManager(DAOFactory.getUtilisateurDAO());
+		}
+		return instance;
+	}
+	
+	// LES FONCTIONS DE GESTION UTILISATEUR
+	
+	// CREER UN NOUVEAU UTILISATEUR
+	public void creerUtilisateur(Utilisateur utilisateur) {
+		utilisateurDAO.insert(utilisateur);
+	}
+	
+	//LOGIN
+	//commenté le temps de mettre login dans le DAO
+	
+	/*public Utilisateur login(String email,String password)
+	{
+		return utilisateurDAO.login(email,password);
+	}*/
+	
+	//SUPPRIMER UTILISATEUR
+	public void deleteById(int id) {
+		utilisateurDAO.delete(id);
+	}
+	
+	//MODIFIER UTILISATEUR
+	//commenté le temps de mettre modifier dans le DAO
+	
+	/*public void modifier(Utilisateur utilisateur) {
+		utilisateurDAO.update(utilisateur);
+	}*/
+}
---
+++ ProjetEnchereMB/src/main/java/fr/eni/enchere/dal/DAOFactory.java
@@ -0,0 +1,11 @@
+package fr.eni.enchere.dal;
+
+import fr.eni.enchere.dal.UtilisateurDAOJdbcImpl;
+
+public class DAOFactory {
+	
+	public static UtilisateurDAO getUtilisateurDAO() {
+		return new UtilisateurDAOJdbcImpl();
+	}
+
+}
