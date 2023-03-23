995b42c4fdc9dee47fed067ed6426552be1b729c
Jammilla <jamila.aitazza2022@campus-eni.fr>
2023-03-23 15:45:15 +0100

    BllException modifier

--- ProjetEnchereMB/src/main/java/fr/eni/enchere/exceptions/BllException.java
+++ ProjetEnchereMB/src/main/java/fr/eni/enchere/exceptions/BllException.java
@@ -1,18 +1,30 @@
 package fr.eni.enchere.exceptions;
 
 public class BllException extends Exception{
-	private static final long serialVersionUID = 1L;
 	
-	public BllException() {
-		super();
-	}
-	
-	public BllException(String message) {
-		super(message);
-	}
-	
-	public String getMessage() {
-		return super.getMessage();
+		public BllException() {
+			super();
+		}
+		
+		public BllException(String message) {
+			super(message);
+		}
+		
+		public BllException(String message, Throwable exception) {
+			super(message, exception);
+		}
+
+		//Méthodes
+		@Override
+		public String getMessage() {
+			StringBuffer sb = new StringBuffer("Couche Bll - ");
+			sb.append(super.getMessage());
+			
+			return sb.toString() ;
+		}
+
+		
 		
 	}
-}
+
+
